6678d22077cc188e424b4fb548c0fd7ee18e68c8
BenjaminPersick <benpersick@gmail.com>
2024-07-21 22:54:09 -0500

    Implement kick message

--- src/main/java/com/benpersick/dmcalerts/MinecraftListener.java
+++ src/main/java/com/benpersick/dmcalerts/MinecraftListener.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import net.dv8tion.jda.api.JDA;
 
@@ -78,5 +79,36 @@ public class MinecraftListener implements Listener {
 		
 		// send message
 		bot.getGuildById(serverID).getTextChannelById(channelID).sendMessage("*" + event.getPlayer().getName() + " left the game*").queue();
+	}
+	
+	@EventHandler
+	public void onPlayerKick(PlayerKickEvent event) {
+	    // attempt to retrieve bot
+        @Nullable
+        JDA bot = DiscordMinecraftAlertsPlugin.getDiscordBot();
+        
+        if (bot == null) {
+            Bukkit.getLogger().log(Level.WARNING, "Unable to send kick message: JDA not initialized.");
+            return;
+        }
+        
+        // attempt to retrieve server id
+        long serverID = prefs.getLong(DiscordMinecraftAlertsPlugin.DISCORD_SERVER_ID_KEY, 0L);
+        
+        if (serverID == 0) {
+            Bukkit.getLogger().log(Level.WARNING, "Unable to send kick message: Discord server ID not set.");
+            return;
+        }
+        
+        // attempt to retrieve channel id
+        long channelID = prefs.getLong(DiscordMinecraftAlertsPlugin.DISCORD_CHANNEL_ID_KEY, 0L);
+        
+        if (channelID == 0) {
+            Bukkit.getLogger().log(Level.WARNING, "Unable to send kick message: Discord channel ID not set.");
+            return;
+        }
+        
+        // send message
+        bot.getGuildById(serverID).getTextChannelById(channelID).sendMessage("*" + event.getPlayer().getName() + " was kicked for " + event.getReason() + "*").queue();
 	}
 }
